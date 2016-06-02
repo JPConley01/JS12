4190ea2e7eff61549d36eaf491bce5e073204515
John Conley <JohnPaul@Johns-MacBook-Pro.local>
2016-06-02 16:51:35 -0600

    changed to angular

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-function getCoordinates(event) {
+angular.module('goldRushApp', []);
+
+angular.module('goldRushApp');
+
+angular.module('goldRushApp').controller('goldCtrl', goldFunc);
+
+    function goldFunc() {
+      console.log("Made it through");
+    }
+
+
+
+
+
+
+/* function getCoordinates(event) {
     var cX = event.clientX;
     var sX = event.screenX;
     var cY = event.clientY;
     var sY = event.screenY;
     console.log(cX);
-}
+    var bullseye = document.getElementById('bullseye');
+    var icon = document.createElement('img');
+    CSSStyleDeclaration
+}*/
